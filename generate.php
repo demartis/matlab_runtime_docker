8868a2887940a1909b52a4f8d16d7f20ab90678f
Riccardo De Martis <riccardo@demartis.it>
2021-01-27 16:46:52 +0100

    feat: script for meshlab

--- generate.php
+++ generate.php
@@ -11,6 +11,7 @@ const R_2019_B = 'R2019b';
 const R_2020_A = 'R2020a';
 
 const FILE_LATEST = 'latest';
+const FILE_LATEST_M = 'latest-meshlab';
 
 $versions = [
     R_2019_A => ['v9.6'],
@@ -74,7 +75,7 @@ function gen_dockerfile($vers, $named_vers, $date, $update_vers, $ld_lib_ver)
 #
 # See https://www.mathworks.com/products/compiler/matlab-runtime.html for more info.
 #
-# @author Riccardo De Martis $date
+# @author Riccardo De Martis$date
 #
 
 FROM debian:stretch-slim
@@ -110,8 +111,56 @@ EOF;
     return $dockerfile;
 }
 
+function gen_dockerfile_meshlab($vers, $named_vers, $date, $tag_name){
+
+$dockerfile = <<<EOF
+# MATLAB Compiler Runtime (MCR) $vers ($named_vers)
+#
+# This docker file will configure an environment into which the Matlab compiler
+# runtime will be installed and in which stand-alone matlab routines (such as
+# those created with MATLAB's deploytool) can be executed.
+
+# MATLAB Runtime
+# Run compiled MATLAB applications or components without installing MATLAB
+# The MATLAB Runtime is a standalone set of shared libraries that enables the
+# execution of compiled MATLAB applications or components. When used together,
+# MATLAB, MATLAB Compiler, and the MATLAB Runtime enable you to create and distribute
+# numerical applications or software components quickly and securely.
+#
+# See https://www.mathworks.com/products/compiler/matlab-runtime.html for more info.
+#
+#
+# MeshLab
+# the open source system for processing and editing 3D triangular meshes.
+# It provides a set of tools for editing, cleaning, healing, inspecting, rendering,
+# texturing and converting meshes.  It offers features for processing raw data produced by
+# 3D digitization tools/devices and for preparing models for 3D printing.
+#
+#
+# @author Riccardo De Martis$date
+#
+
+FROM demartis/matlab-runtime:$tag_name
+MAINTAINER Riccardo De Martis <riccardo@demartis.it>
+
+RUN apt-get -q update && \
+    apt-get install -q -y --no-install-recommends \
+      meshlab && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+EOF;
+
+    return $dockerfile;
+}
+
 function folder_name($d_name, $d_updv){
    return $d_name.'-u'.$d_updv;
+}
+
+function safe_link($target, $link){
+    unlink($link);
+    return symlink($target, $link);
 }
 
 // Generate Dockerfiles
@@ -131,18 +180,26 @@ foreach($builds as $version=>$build){
             $d_date = '';
         }
 
+        // create dockerfile
         $dockerfile = gen_dockerfile($d_vers, $d_name, $d_date, $d_updv, $d_libv);
 
         $folder_name = folder_name($d_name, $d_updv);
         if (!file_exists($folder_name)) {
-            mkdir($folder_name, 0777, true);
+            mkdir($folder_name, 0775, true);
+        }
+        file_put_contents($folder_name.DIRECTORY_SEPARATOR.'Dockerfile', $dockerfile);
+
+        // create dockerfile with Meshlab
+        $dockerfile = gen_dockerfile_meshlab($d_vers, $d_name, $d_date, $folder_name);
+        $folder_name .= '-meshlab';
+        if (!file_exists($folder_name)) {
+            mkdir($folder_name, 0775, true);
         }
         file_put_contents($folder_name.DIRECTORY_SEPARATOR.'Dockerfile', $dockerfile);
     }
 }
 
 // Link latest versions
-
 foreach($builds as $version=>$build){
 
     $update = end($build);
@@ -150,7 +207,12 @@ foreach($builds as $version=>$build){
 
     $folder_name = folder_name($d_name, $d_updv);
 
-    unlink(FILE_LATEST);
-    symlink($folder_name, FILE_LATEST);
-
+    // link main versions
+    safe_link($folder_name, $version);
+    safe_link($folder_name.'-meshlab', $version.'-meshlab');
 }
+
+// link latest latest-meshlab
+$last_version = end(array_keys($versions));
+safe_link($last_version, FILE_LATEST);
+safe_link($last_version.'-meshlab', FILE_LATEST_M);
